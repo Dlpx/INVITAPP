c09902f48b843b877a4a4c97961b181ce031dcf9
Nicolas De Carlo <kinesiologodecarlo@gmail.com>
2022-11-17 10:34:35 -0500

    Creacion de invitado en JS

--- script.js
+++ script.js
@@ -21,21 +21,86 @@ botonesMenu.forEach(elemento =>
 let listaInvitados = [];
 
 //Variables formulario:
-let nombreInvitado = document.getElementById("nombreInvitado");
-let atNombre = document.getElementById("atNombre");
+let numeroActualizado;
+let avatarInvitado;
+let nombreInvitado;
+let llevaInvitado;
+let gastoInvitado;
+let correoInvitado;
 
+//Variables TARJETA
+let atNumero;
+let atAvatar;
+let atNombre;
+let atLleva;
+let atGasto;
+let atCorreo;
 
-//Actualizacion nombre y numero
-nombreInvitado.addEventListener("input", () => {
-    let numeroActualizado = listaInvitados.length + 1;
-    let atNumero = document.getElementById("atNumero");
-    atNumero.innerText=numeroActualizado;
-    atNombre.innerText="";
-    //actualizacion AVATAR
-    let avatarInvitado = document.getElementById("avatarInvitado");
+//actualizacion AVATAR
+avatarInvitado = document.getElementById("avatarInvitado");
+avatarInvitado.addEventListener("change", () => {
     let avatar = avatarInvitado.options[avatarInvitado.selectedIndex].text;
-    let atAvatar = document.getElementById("atAvatar");
-    atNombre.innerText=nombreInvitado.value;
-    atAvatar.innerHTML=`<img src="./img/Usuarios/${avatar}.png" alt="tipo de usuario">`
+    atAvatar = document.getElementById("atAvatar");
+    atAvatar.innerHTML=`<img src="./img/Usuarios/${avatar}.png" alt="tipo de usuario">`;
 });
 
+//Actualizacion nombre y numero
+nombreInvitado = document.getElementById("nombreInvitado");
+nombreInvitado.addEventListener("input", () => {
+    numeroActualizado = listaInvitados.length + 1;
+    atNumero = document.getElementById("atNumero");
+    atNumero.innerText=parseInt(numeroActualizado);
+    atNombre = document.getElementById("atNombre");
+    atNombre.innerText="";
+    atNombre.innerText=nombreInvitado.value;
+});
+
+//Actualizacion de ITEM
+llevaInvitado = document.getElementById("llevaInvitado");
+atLleva = document.getElementById("atLleva");
+llevaInvitado.addEventListener("input", () =>{
+    atLleva.innerText = llevaInvitado.value;
+});
+
+//Actualizacion de GASTO
+atGasto = document.getElementById("atGasto");
+gastoInvitado = document.getElementById("gastoInvitado");
+gastoInvitado.addEventListener("input", () => {
+    atGasto.innerText = `$ ${gastoInvitado.value}`;
+});
+
+//Actualizacion de eMail
+atCorreo = document.getElementById("atCorreo");
+correoInvitado = document.getElementById("correoInvitado");
+correoInvitado.addEventListener("input", () => {
+    atCorreo.innerHTML = correoInvitado.value;
+});
+
+// ----- GUARDADO EN LISTAINVITADOS ----- //
+//OBJETO PERSONA
+
+//Constructor:
+class Persona {
+    constructor(numero, avatar, nombre, item, gasto, correo){
+        this.numero = numero;
+        this.avatar = avatar;
+        this.nombre = nombre;
+        this.item = item;
+        this.gasto = gasto;
+        this.correo = correo;
+    }
+}
+let btnAgregar = document.getElementById("btnAgregar");
+btnAgregar.addEventListener("click", () =>{
+    let nuevoInvitado = new Persona(Number(atNumero.innerText), atAvatar.innerHTML, atNombre.innerText, atLleva.innerText, Number(gastoInvitado.value), atCorreo.innerText);
+    listaInvitados.push(nuevoInvitado);
+    
+    //Reseteo de tarjeta
+    atNumero.innerText = "N°";
+    atAvatar.innerHTML = `<img src="./img/Usuarios/Ninguno.png" alt="tipo de usuario">`;
+    atNombre.innerText = "Nombre";
+    atLleva.innerText = "Item";
+    atGasto.innerHTML = "$ Gasto";
+    atCorreo.innerText = "correo@correo.com";
+   
+});
