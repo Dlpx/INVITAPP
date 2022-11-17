c30676e5ec7c25881b3157c8074c8ad8663166ea
Nicolas De Carlo <kinesiologodecarlo@gmail.com>
2022-11-16 22:38:35 -0500

    Avances en el form de la tarjeta, falta coordinar la forma de cambiar la imagen

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ botonDesplegar.addEventListener("click", menuDesplegable);
 logoBtn.addEventListener("click", menuDesplegable);
 botonesMenu.forEach(elemento => 
     elemento.addEventListener("click", menuDesplegable));
+
+// ----- SECCION AÑADIR INVITADO ----- //
+let listaInvitados = [];
+
+//Variables formulario:
+let nombreInvitado = document.getElementById("nombreInvitado");
+let atNombre = document.getElementById("atNombre");
+
+
+//Actualizacion nombre y numero
+nombreInvitado.addEventListener("input", () => {
+    let numeroActualizado = listaInvitados.length + 1;
+    let atNumero = document.getElementById("atNumero");
+    atNumero.innerText=numeroActualizado;
+    atNombre.innerText="";
+    //actualizacion AVATAR
+    let avatarInvitado = document.getElementById("avatarInvitado");
+    let avatar = avatarInvitado.options[avatarInvitado.selectedIndex].text;
+    let atAvatar = document.getElementById("atAvatar");
+    atNombre.innerText=nombreInvitado.value;
+    atAvatar.innerHTML=`<img src="./img/Usuarios/${avatar}.png" alt="tipo de usuario">`
+});
+
